08e076b206bd84b02eeea2e28d5ba7f9f3b86984
prithvikochhar <prkochhar@ucsd.edu>
2025-04-25 19:08:35 -0700

    JS 2 Commit Final 1

--- global.js
+++ global.js
@@ -85,5 +85,35 @@ const BASE_PATH =
       if ("colorScheme" in localStorage) {
         setColorScheme(localStorage.colorScheme);
       }
-
+      export async function fetchJSON(url) {
+        try {
+          // Fetch the JSON file from the given URL
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+          }
+          const data = await response.json();
+          return data;
+          }
+         catch (error) {
+          console.error('Error fetching or parsing JSON data:', error);
+        }
+      }
+      export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+        // Your code will go here
+        containerElement.innerHTML = '';
+        for (let project of projects){
+          const article = document.createElement('article');
+          article.innerHTML = `<${headingLevel}>${project.title}</${headingLevel}>
+          <img src="${project.image}" alt="${project.title}">
+          <p>${project.description}</p>
+          `;
+          containerElement.appendChild(article);
+        }
+      }
+      export async function fetchGitHubData(username) {
+        // return statement here
+        return fetchJSON(`https://api.github.com/users/${username}`);
+      }
+      
       
