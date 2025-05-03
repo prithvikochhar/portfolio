06a29061bda1aa37b936f5e5642db5e9d98f0b83
prithvikochhar <prkochhar@ucsd.edu>
2025-05-02 21:51:11 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -18,14 +18,22 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 // let data = [1, 2];
 // let data = [1, 2];
 // let data = [1, 2, 3, 4, 5, 5];
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+// let data = [
+//   { value: 1, label: 'apples' },
+//   { value: 2, label: 'oranges' },
+//   { value: 3, label: 'mangos' },
+//   { value: 4, label: 'pears' },
+//   { value: 5, label: 'limes' },
+//   { value: 5, label: 'cherries' },
+// ];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 let sliceGenerator = d3.pie().value((d) => d.value);
@@ -67,6 +75,7 @@ data.forEach((d, idx) => {
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
     
 });
+
 
 
 
