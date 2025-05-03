eacb5d921d0b92f7950dc66ab1abd775210d3134
prithvikochhar <prkochhar@ucsd.edu>
2025-05-02 21:45:06 -0700

    Lab 5 Commit 2

--- projects/projects.js
+++ projects/projects.js
@@ -17,9 +17,19 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 // let data = [1, 2];
 // let data = [1, 2];
-let data = [1, 2, 3, 4, 5, 5];
+// let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
+// let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 let total = 0;
@@ -48,6 +58,15 @@ arcs.forEach((arc, idx) => {
     .attr('d', arc)
     .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
 })
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+    
+});
 
 
 
