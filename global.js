52bf7689aa353964e9ae989ae599acf98570eea3
prithvikochhar <prkochhar@ucsd.edu>
2025-05-10 21:51:50 -0700

    Lab 6 Commit 1

--- global.js
+++ global.js
@@ -16,6 +16,7 @@ let pages = [
   { url: "projects/", title: "Projects" },
   { url: "contact/", title: "Contact" },
   { url: "resume/", title: "Resume" },
+  {url : "meta/", title: "Meta"},
   { url: "https://github.com/prithvikochhar", title: "GitHub" }
 ];
 
