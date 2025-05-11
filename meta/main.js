73b921e54edbd94ece935e84d3331dcbb82bfbc5
prithvikochhar <prkochhar@ucsd.edu>
2025-05-10 22:37:19 -0700

    Lab 6 Commit Step 2

--- meta/main.js
+++ meta/main.js
@@ -77,16 +77,23 @@ function renderCommitInfo(data, commits) {
   dl.append('dt').text('Avg. line length (chars)');
   dl.append('dd').text(avgLineLength);
   }
-
   let data = await loadData();
-  let commits = processCommits(data);
-  
+let commits = processCommits(data);
   renderCommitInfo(data, commits);
 
   function renderScatterPlot(data, commits) {
     // Put all the JS code of Steps inside this function
     const width = 1000;
     const height = 600;
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+      };
 
     const svg = d3
     .select('#chart')
@@ -97,10 +104,10 @@ function renderCommitInfo(data, commits) {
     const xScale = d3
     .scaleTime()
     .domain(d3.extent(commits, (d) => d.datetime))
-    .range([0, width])
+    .range([usableArea.left, usableArea.right])
     .nice();
 
-    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+    const yScale = d3.scaleLinear().domain([0, 24]).range([usableArea.bottom, usableArea.top]);
 
     const dots = svg.append('g').attr('class', 'dots');
 
@@ -112,9 +119,31 @@ function renderCommitInfo(data, commits) {
     .attr('cy', (d) => yScale(d.hourFrac))
     .attr('r', 5)
     .attr('fill', 'steelblue');
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale).tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+    // Create gridlines as an axis with no labels and full-width ticks
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+    // Add X axis
+    svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+    // Add Y axis
+    svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
   }
-  
-  
-  
-  renderScatterPlot(data, commits);
+
+renderScatterPlot(data,commits);
 
